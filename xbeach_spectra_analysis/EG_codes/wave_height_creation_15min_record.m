6ccc16493b5886786348815e43617d3fda1e79a6
Tarandeep Kalra <tkalra@whgrp.local>
2024-09-11 14:53:11 -0400

    added comments

--- xbeach_spectra_analysis/EG_codes/wave_height_creation_15min_record.m
+++ xbeach_spectra_analysis/EG_codes/wave_height_creation_15min_record.m
@@ -6,11 +6,16 @@ clear all
 ncfile='C:\Users\eday\Documents\XBeach\NWFW_NC_Oriental\Trial_Models\trial_model9\xboutput.nc';
 %ncinfo=ncinfo(ncfile);
 t=ncread(ncfile,'globaltime');
+% Get the x values for the transect
 x=ncread(ncfile,'globalx');
-zs=ncread(ncfile,'zs');
+% Get the surface water elevation 
+zs=ncread(ncfile,'zs'); 
+% Transpose to [time, elevation] array format
 zs=squeeze(zs)';
-zb=ncread(ncfile,'zb');
+% Bed profile 
+zb=ncread(ncfile,'zb'); 
 zb=squeeze(zb)';
+% Transpose x 
 x=x',
 
 %wave record 15 minute time period is 900sec.
@@ -59,8 +64,10 @@ Hm0{j}=Hm0_i;
 
 zs_avg{j}=zs_avgi2;
 zs_reg{j}=zs_regi;
-end
+end 
+
 bed=zb(1,1:end);
+
 t_int=[];
 for jj=1:delta_t
     n=jj
@@ -68,55 +75,7 @@ for jj=1:delta_t
     tstr=datestr(seconds(t1),'DD HH:MM:SS')
     t_int{jj}=tstr
 end
+
+% Save the data in chunks 
 save('Transect1_trial9.mat','bed','x','Hm0','zs_avg','zs_reg','t_int','zs');
-%%
-close all
-clear all
-[num dat all]=xlsread('jonswap_100yr.xls');
-clear dat all
-wh_js=num(1:380,1)';
-
-load Transect1_trial9.mat
-figure(1)
-axis tight manual
-set(gca)
-%set(gcf, 'Position', get(0, 'Screensize'));
-for ii=1:length(Hm0)
-    t=t_int{1,ii}
-    p1=plot(x,bed,'k')
-    hold on
-    p2=plot(x,Hm0{1,ii},'r')
-    hold on
-    p3=plot(x,zs_avg{1,ii},'g')
-    hold on 
-    p4=plot(x,zs_reg{1,ii},'b')
-    hold on 
-    p5=plot(-1.345073035543540e+02,wh_js(ii),'ro')
-    title_n=strcat('Time',{' '},t);
-    title(title_n)
-    ylim([-3 3])
-    xlim([0 250])
-    legend([p1 p2 p3 p4 p5],{'Bedlevel','Hm0','Mean Water Level','Instaneous Water Level','Input Wave Height (JONSWAP)'},'Location','southeast')
-    F(ii)=getframe(gcf)
-    pause(0.5)
-    delete(p2)
-    delete(p3)
-    delete(p4)
-    delete(p1)
-    delete(p5) 
-end
-% create the video writer with 1 fps
-  writerObj = VideoWriter('transect2_rd_int_15wr.avi');
-  writerObj.FrameRate = 10;
-  % set the seconds per image
-% open the video writer
-open(writerObj);
-% write the frames to the video
-for i=1:length(F)
-    % convert the image to a frame
-    frame = F(i) ;    
-    writeVideo(writerObj, frame);
-end
-% close the writer object
-close(writerObj);
-
+%% 
